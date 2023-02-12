6f6979ee2953d15975aa24b1d68f25e7c2c2f7f0
Ulascan Senturk <45449532+ulascansenturk@users.noreply.github.com>
2023-02-12 23:39:37 +0300

    Tech update long lat, gather long url from short url, other improvements, healthcheck endpoint (#1)
    * Gathering long,lat from short urls, other additions
    
    latitude & longitude extraction from shortened google maps url are done
    healthcheck endpoint are done

--- util/util.go
+++ util/util.go
@@ -3,6 +3,10 @@ package util
 import (
 	"hash/fnv"
 	"math/rand"
+	"net/http"
+	"regexp"
+
+	"github.com/sirupsen/logrus"
 )
 
 func Hash(s string) uint32 {
@@ -19,4 +23,40 @@ func RandomString(n int) string {
 		b[i] = letterRunes[rand.Intn(len(letterRunes))]
 	}
 	return string(b)
+}
+
+func URLtoLatLng(url string) map[string]string {
+	re := regexp.MustCompile(`!3d(-?\d+.\d+)`)
+	lat := re.FindStringSubmatch(url)[1]
+	re = regexp.MustCompile(`!4d(-?\d+.\d+)`)
+	lng := re.FindStringSubmatch(url)[1]
+	return map[string]string{
+		"lat": lat,
+		"lng": lng,
+	}
+}
+
+// Gathering the long URL from the short google maps url
+func GatherLongUrlFromShortUrl(shortURL string) (string, error) {
+
+	client := &http.Client{}
+	res, err := client.Get(shortURL)
+	if err != nil {
+		logrus.Error(err)
+		return "", err
+	}
+	defer res.Body.Close()
+	fullURL := res.Request.URL.String()
+	for res.StatusCode == 302 || res.StatusCode == 301 {
+		res, err = client.Get(fullURL)
+		if err != nil {
+			logrus.Error(err)
+			return "", err
+		}
+		defer res.Body.Close()
+		fullURL = res.Request.URL.String()
+
+	}
+	return fullURL, nil
+
 }
